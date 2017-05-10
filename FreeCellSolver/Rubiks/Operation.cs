e1db90ae95050f13829f0f58a747bc3f855c0b07
quanb <quanb@DESKTOP-2SN0J3G>
2017-05-11 01:39:04 +1000

    added singmaster notation to operation.

--- FreeCellSolver/Rubiks/Operation.cs
+++ FreeCellSolver/Rubiks/Operation.cs
@@ -10,6 +10,28 @@ namespace Rubiks
     {
         public class Operation : IEquatable<Operation>, IOperation
         {
+            public enum Singmaster
+            {
+                F,
+                B,
+                U,
+                D,  
+                L,
+                R,
+                F2,
+                B2,
+                U2,
+                D2,
+                L2,
+                R2,
+                Fp,
+                Bp,
+                Up,
+                Dp,
+                Lp,
+                Rp
+            }
+
             public enum DiscAxes
             {
                 X = 0,
@@ -29,20 +51,171 @@ namespace Rubiks
             public int DiscIndex;  // 0, 1, ..., n/2-1, n/2+1, ... n
             public Angles Angle;
 
+            public Operation()
+            {
+            }
+
+            public Operation(Singmaster sm)
+                => FromSingmaster(sm);
+            
             public override string ToString()
-                =>$"{DiscAxis},{DiscIndex},{Angle}";
+                => $"{DiscAxis},{DiscIndex},{Angle}";
 
             public override bool Equals(object obj)
                 => (obj as Operation)?.Equals(this) ?? false;
 
             public override int GetHashCode()
-             => HashingHelper.GetHashCodeForItems(DiscAxis, DiscIndex, Angle);
+                => HashingHelper.GetHashCodeForItems(DiscAxis, DiscIndex, Angle);
 
             public Operation Clone()
                 => new Operation { DiscAxis = DiscAxis, DiscIndex = DiscIndex, Angle = Angle };
 
             public bool Equals(Operation other)
                 => DiscAxis == other.DiscAxis && DiscIndex == other.DiscIndex && Angle == other.Angle;
+
+            public static implicit operator Singmaster(Operation op)
+                => op.ToSingmaster();
+
+            public static implicit operator Operation(Singmaster sm)
+                => new Operation(sm);
+
+            public Singmaster ToSingmaster()
+            {
+                switch (DiscAxis)
+                {
+                    case DiscAxes.X:
+                        switch (Angle)
+                        {
+                            case Angles.Forward:
+                                return DiscIndex == 1 ? Singmaster.L : Singmaster.Rp;
+                            case Angles.Backward:
+                                return DiscIndex == 1 ? Singmaster.Lp : Singmaster.R;
+                            case Angles.Mirror:
+                                return DiscIndex == 1 ? Singmaster.L2 : Singmaster.R2;
+                        }
+                        break;
+                    case DiscAxes.Y:
+                        switch (Angle)
+                        {
+                            case Angles.Forward:
+                                return DiscIndex == 1 ? Singmaster.F : Singmaster.Bp;
+                            case Angles.Backward:
+                                return DiscIndex == 1 ? Singmaster.Fp : Singmaster.B;
+                            case Angles.Mirror:
+                                return DiscIndex == 1 ? Singmaster.F2 : Singmaster.B2;
+                        }
+                        break;
+                    case DiscAxes.Z:
+                        switch (Angle)
+                        {
+                            case Angles.Forward:
+                                return DiscIndex == 1 ? Singmaster.B : Singmaster.Up;
+                            case Angles.Backward:
+                                return DiscIndex == 1 ? Singmaster.Bp : Singmaster.U;
+                            case Angles.Mirror:
+                                return DiscIndex == 1 ? Singmaster.B2 : Singmaster.U2;
+                        }
+                        break;
+                }
+                throw new ArgumentException("Unexpected operation");
+            }
+
+            public void FromSingmaster(Singmaster sm)
+            {
+                switch (sm)
+                {
+                    case Singmaster.F:
+                        DiscAxis = DiscAxes.Y;
+                        DiscIndex = 1;
+                        Angle = Angles.Forward;
+                        break;
+                    case Singmaster.B:
+                        DiscAxis = DiscAxes.Y;
+                        DiscIndex = 0;
+                        Angle = Angles.Backward;
+                        break;
+                    case Singmaster.U:
+                        DiscAxis = DiscAxes.Z;
+                        DiscIndex = 0;
+                        Angle = Angles.Backward;
+                        break;
+                    case Singmaster.D:
+                        DiscAxis = DiscAxes.Z;
+                        DiscIndex = 1;
+                        Angle = Angles.Forward;
+                        break;
+                    case Singmaster.L:
+                        DiscAxis = DiscAxes.X;
+                        DiscIndex = 1;
+                        Angle = Angles.Forward;
+                        break;
+                    case Singmaster.R:
+                        DiscAxis = DiscAxes.X;
+                        DiscIndex = 0;
+                        Angle = Angles.Backward;
+                        break;
+                    case Singmaster.F2:
+                        DiscAxis = DiscAxes.Y;
+                        DiscIndex = 1;
+                        Angle = Angles.Mirror;
+                        break;
+                    case Singmaster.B2:
+                        DiscAxis = DiscAxes.Y;
+                        DiscIndex = 0;
+                        Angle = Angles.Mirror;
+                        break;
+                    case Singmaster.U2:
+                        DiscAxis = DiscAxes.Z;
+                        DiscIndex = 0;
+                        Angle = Angles.Mirror;
+                        break;
+                    case Singmaster.D2:
+                        DiscAxis = DiscAxes.Z;
+                        DiscIndex = 1;
+                        Angle = Angles.Mirror;
+                        break;
+                    case Singmaster.L2:
+                        DiscAxis = DiscAxes.X;
+                        DiscIndex = 1;
+                        Angle = Angles.Mirror;
+                        break;
+                    case Singmaster.R2:
+                        DiscAxis = DiscAxes.X;
+                        DiscIndex = 0;
+                        Angle = Angles.Mirror;
+                        break;
+                    case Singmaster.Fp:
+                        DiscAxis = DiscAxes.Y;
+                        DiscIndex = 1;
+                        Angle = Angles.Backward;
+                        break;
+                    case Singmaster.Bp:
+                        DiscAxis = DiscAxes.Y;
+                        DiscIndex = 0;
+                        Angle = Angles.Forward;
+                        break;
+                    case Singmaster.Up:
+                        DiscAxis = DiscAxes.Z;
+                        DiscIndex = 0;
+                        Angle = Angles.Forward;
+                        break;
+                    case Singmaster.Dp:
+                        DiscAxis = DiscAxes.Z;
+                        DiscIndex = 1;
+                        Angle = Angles.Backward;
+                        break;
+                    case Singmaster.Lp:
+                        DiscAxis = DiscAxes.X;
+                        DiscIndex = 1;
+                        Angle = Angles.Backward;
+                        break;
+                    case Singmaster.Rp:
+                        DiscAxis = DiscAxes.X;
+                        DiscIndex = 0;
+                        Angle = Angles.Forward;
+                        break;
+                }
+            }
 
             #region IOperation members
 
